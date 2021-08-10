e9e7970290ce279eb2d501b1277fea8fb4fc77dc
HUEN OH <huen.oh@ssense.com>
2021-08-09 23:15:03 -0400

    6_thunk_sample_api_3_loading

--- src/lib/createRequestThunk.js
+++ src/lib/createRequestThunk.js
@@ -6,18 +6,21 @@ export default function createRequestThunk(type, request) {
   const FAILURE = `${type}_FAILURE`;
   return params => async dispatch => {
     dispatch({ type }); // 시작됨
+    dispatch(startLoading(type));
     try {
       const response = await request(params);
       dispatch({
         type: SUCCESS,
         payload: response.data
       }); // 성공
+      dispatch(finishLoading(type));
     } catch (e) {
       dispatch({
         type: FAILURE,
         payload: e,
         error: true
       }); // 에러 발생
+      dispatch(startLoading(type));
       throw e;
     }
   };
--- src/modules/sample.js
+++ src/modules/sample.js
@@ -5,11 +5,9 @@ import createRequestThunk from '../lib/createRequestThunk';
 // 액션 타입들을 선언합니다.
 const GET_POST = 'sample/GET_POST';
 const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
-const GET_POST_FAILURE = 'sample/GET_POST_FAILURE';
 
 const GET_USERS = 'sample/GET_USERS';
 const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
-const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
 export const getPost = createRequestThunk(GET_POST, api.getPost);
 export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
@@ -18,62 +16,22 @@ export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 // 요청의 로딩중 상태는 loading 이라는 객체에서 관리합니다.
 
 const initialState = {
-    loading: {
-        GET_POST: false,
-        GET_USERS: false
-    },
     post: null,
     users: null
-};
-
-const sample = handleActions(
-  {
-    [GET_POST]: state => ({
+  };
+  
+  const sample = handleActions(
+    {
+      [GET_POST_SUCCESS]: (state, action) => ({
         ...state,
-        loading: {
-            ...state.loading,
-            GET_POST: true
-        }
-    }),
-    [GET_POST_SUCCESS]: (state, action) => ({
-        ...state,
-        loading: {
-            ...state.loading,
-            GET_POST: false
-        },
         post: action.payload
-    }),
-    [GET_POST_FAILURE]: (state, action) => ({
+      }),
+      [GET_USERS_SUCCESS]: (state, action) => ({
         ...state,
-        loading: {
-            ...state.loading,
-            GET_POST: false
-        },
-    }),
-    [GET_USERS]: state => ({
-        ...state,
-        loading: {
-            ...state.loading,
-            GET_USERS: true
-        }
-    }),
-    [GET_USERS_SUCCESS]: (state, action) => ({
-        ...state,
-        loading: {
-            ...state.loading,
-            GET_USERS: false
-        },
         users: action.payload
-    }),
-    [GET_USERS_FAILURE]: (state, action) => ({
-        ...state,
-        loading: {
-            ...state.loading,
-            GET_USERS: false
-        },
-    })
-  },
-  initialState
-);
+      })
+    },
+    initialState
+  );
 
 export default sample;
