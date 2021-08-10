f25d87165af4977cb3a2064c39704735848e0f8c
HUEN OH <huen.oh@ssense.com>
2021-08-09 22:59:16 -0400

    5_thunk_sample_api_2_createRequestThrunk

--- src/modules/sample.js
+++ src/modules/sample.js
@@ -1,5 +1,6 @@
 import { handleActions } from 'redux-actions';
 import * as api from '../lib/api';
+import createRequestThunk from '../lib/createRequestThunk';
 
 // 액션 타입들을 선언합니다.
 const GET_POST = 'sample/GET_POST';
@@ -10,41 +11,8 @@ const GET_USERS = 'sample/GET_USERS';
 const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
 const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
-export const getPost = id => async dispatch => {
-    dispatch({ type: GET_POST});
-    try {
-        const response = await api.getPost(id);
-        dispatch({
-            type: GET_POST_SUCCESS,
-            payload: response.data
-        });
-    } catch (e) {
-        dispatch({
-            type: GET_POST_FAILURE,
-            payload: e,
-            error: true
-         });
-        throw e;
-    }
-};
-
-export const getUsers = () => async dispatch => {
-    dispatch({ type: GET_USERS});
-    try {
-        const response = await api.getUsers();
-        dispatch({
-            type: GET_USERS_SUCCESS,
-            payload: response.data
-        });
-    } catch (e) {
-        dispatch({
-            type: GET_USERS_FAILURE,
-            payload: e,
-            error: true
-         });
-        throw e;
-    }
-}
+export const getPost = createRequestThunk(GET_POST, api.getPost);
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 
 // 초기 상태를 선언합니다.
 // 요청의 로딩중 상태는 loading 이라는 객체에서 관리합니다.
