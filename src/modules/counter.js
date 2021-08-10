9106ba640c298cdaa81482a10bd0908e098e65a5
HUEN OH <huen.oh@ssense.com>
2021-08-10 00:52:44 -0400

    11_saga_tips

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,5 +1,5 @@
 import { createAction, handleActions } from 'redux-actions';
-import { delay, put, takeEvery, takeLatest } from 'redux-saga/effects';
+import { delay, put, throttle, takeLatest, select } from 'redux-saga/effects';
 
 
 const INCREASE = 'counter/INCREASE';
@@ -17,6 +17,8 @@ export const decreaseAsync = createAction(DECREASE_ASYNC, () => undefined);
 function* increaseSaga() {
   yield delay(1000); // 1초를 기다립니다.
   yield put(increase()); // 특정 액션을 디스패치 합니다.
+  const number = yield select(state => state.counter);
+  console.log(`현재 값은 ${number}입니다.`);
 }
 
 function* decreaseSaga() {
@@ -25,8 +27,8 @@ function* decreaseSaga() {
 }
 
 export function* counterSaga() {
-  // takeEvery 는 들어오는 모든 액션에 대하여 특정 작업을 처리해줍니다.
-  yield takeEvery(INCREASE_ASYNC, increaseSaga);
+  // 첫번째 파라미터: n초 * 1000
+  yield throttle(3000, INCREASE_ASYNC, increaseSaga);
   // takeLatest 는 만약 기존에 진행중이던 작업이 있다면 취소처리 하고
   // 가장 마지막으로 실행된 작업만을 수행합니다.
   yield takeLatest(DECREASE_ASYNC, decreaseSaga);
